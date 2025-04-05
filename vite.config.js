6bc7fc9e9fc4dbf11c29a337051c38baa08ea4a3
Magdelene Phua <magdelene_phua@outlook.com>
2025-04-05 10:39:17 +0800

    test 3

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-react-app/',
+  base: '/currency-converter/',
   test: {
     globals: true,
     environment: 'jsdom',
